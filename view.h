d5d00ee3520c4f473e30322de59fed686567b0a2
ZYROS-OFFICIEL <rondomiez@gmail.com>
2025-10-11 23:42:44 +0100

    adding squeeze

--- view.h
+++ view.h
@@ -12,3 +12,15 @@ Tensor Tensor::reshape(const std::vector<size_t>& new_shape) const {
     out.shape = new_shape;
     return out;
 }
+Tensor Tensor::squeeze() const {
+    std::vector<size_t> new_shape;
+    for (auto s : shape)
+        if (s != 1) new_shape.push_back(s);
+
+    // If all dims were 1 → keep a single 1D scalar shape {}
+    if (new_shape.empty()) new_shape.push_back(1);
+
+    Tensor out = *this;
+    out.shape = new_shape;
+    return out;
+}
