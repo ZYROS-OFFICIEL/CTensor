b4085b42f3eca18fdb52ba45ed30057111912d5c
ZYROS-OFFICIEL <rondomiez@gmail.com>
2025-10-17 23:58:06 +0100

    adding new tensor implementation

--- tensor1.h
+++ tensor1.h
@@ -9,6 +9,7 @@
 #include <stdexcept>    // exceptions
 #include <cassert>
 
+using namespace std;
 enum class DType { Float32, Int32, Double64 };
 static size_t dtype_size(DType dt) {
     switch (dt) {
@@ -17,4 +18,16 @@ static size_t dtype_size(DType dt) {
         case DType::Double64:return sizeof(double);
     }
     return sizeof(float);
+}
+
+struct Storage{
+    std::shared_ptr<void> data;
+    size_t size;
+    static shared_ptr<void> allocate(size_t n, DType dt){
+        size = n * dtype_size(dt);
+        data = std::shared_ptr<void>(malloc(size), free);
+        if(!data && size) throw std::bad_alloc();
+        memset(data.get(), 0, size);
+        return std::shared_ptr<void>(p, std::free);
+    }
 }
