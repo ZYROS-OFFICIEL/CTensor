cbc41f4bc707e96ecc63701c71ff40e75b0d22d3
ZYROS-OFFICIEL <rondomiez@gmail.com>
2025-11-06 00:04:18 +0100

    Tan op

--- ops1.cpp
+++ ops1.cpp
@@ -690,6 +690,29 @@ Tensor cos_(const Tensor& a_) {
 
     return result;
 }
+Tensor tan_(const Tensor& a_) {
+    if (!a_.impl)
+        throw std::runtime_error("cos_: null tensor implementation");
+
+    size_t n = a_.numel_();  // total number of elements
+    bool req = a_.requires_grad();
+
+    Tensor result(a_.impl->shape, a_.impl->dtype, req);
+
+    if (req) {
+        result.impl->grad_fn = std::make_shared<GradTan>(a_);
+    }
+
+    auto* a_data = a_.impl->storage->data.get();
+    auto* r_data = result.impl->storage->data.get();
+
+    for (size_t i = 0; i < n; ++i) {
+        double val = read_scalar_at(a_data, i, a_.impl->dtype);
+        write_scalar_at(r_data, i, result.impl->dtype, std::tan(val));
+    }
+
+    return result;
+}
 
 
 
