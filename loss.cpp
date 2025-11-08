454afde94d46373605c051c572720176dd5653db
ZYROS-OFFICIEL <rondomiez@gmail.com>
2025-11-08 02:20:40 +0100

    defining CrossEntropy

--- loss.cpp
+++ loss.cpp
@@ -29,6 +29,19 @@ Tensor Loss::MSE(const Tensor& pred_, const Tensor& target_) {
 
     return result;
 }
+Tensor Loss::CrossEntropy(const Tensor& pred_, const Tensor& target_) {
+        if (!pred_.impl || !target_.impl)
+        throw std::runtime_error("Loss::CrossEntropy: null tensor implementation");
+
+    if (pred_.impl->ndim != target_.impl->ndim)
+        throw std::runtime_error("Loss::CrossEntropy: dimension mismatch");
+        
+    bool req = pred_.requires_grad();
+    Tensor result({1}, pred_.impl->dtype, req);
+    result = softmax(pred_, -1);
+    result = -sum(target_ * log_(result), -1);
+    return result;
+}
 
 void GradMSE::backward(const Tensor& self) {
     if (!self.impl || !self.impl->storage || !self.impl->storage->grad)
