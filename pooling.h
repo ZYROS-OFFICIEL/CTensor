984865d31b700f864a7611e9ce48d2a87ddd5775
ZYROS-OFFICIEL <rondomiez@gmail.com>
2025-11-15 23:57:10 +0100

    declaration of avgpooling AvgPool1d AvgPool2d

--- pooling.h
+++ pooling.h
@@ -31,3 +31,39 @@ public:
     Tensor forward(const Tensor& input);
     Tensor operator()(const Tensor& input) { return forward(input); }
 };
+
+class MaxPool3d {
+public:
+    int kernel_size_d, kernel_size_h, kernel_size_w;
+    int stride_d, stride_h, stride_w;
+    int padding_d, padding_h, padding_w;
+
+    MaxPool3d(int kd, int kh, int kw, int sd = 1, int sh = 1, int sw = 1, int pd = 0, int ph = 0, int pw = 0);
+
+    // forward & call operator
+    Tensor forward(const Tensor& input);
+    Tensor operator()(const Tensor& input) { return forward(input); }
+};
+class AvgPool1d {
+public:
+    int kernel_size, stride, padding;
+
+    AvgPool1d(int k, int s = 1, int p = 0);
+
+    // forward & call operator
+    Tensor forward(const Tensor& input);
+    Tensor operator()(const Tensor& input) { return forward(input); }
+};
+
+class AvgPool2d {
+public:
+    int kernel_size_h, kernel_size_w;
+    int stride_h, stride_w;
+    int padding_h, padding_w;
+
+    AvgPool2d(int kh, int kw, int sh = 1, int sw = 1, int ph = 0, int pw = 0);
+
+    // forward & call operator
+    Tensor forward(const Tensor& input);
+    Tensor operator()(const Tensor& input) { return forward(input); }
+};
