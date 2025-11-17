a8c5d26ee71c44141c2eb82796bd2ea4e3348c24
ZYROS-OFFICIEL <rondomiez@gmail.com>
2025-11-17 23:48:51 +0100

    add Relu.h header file with LeakyRelu function declaration

---
+++ Relu.h
@@ -0,0 +1,12 @@
+#include "tensor1.h"
+#include <vector>
+#include <stdexcept>
+#include <cmath>
+#include "autograd.h"
+#include <immintrin.h>
+#include <cstring>
+#include "ops1.h"
+#include <string>
+#pragma once
+
+Tensor LeakyRelu(const Tensor& a_,double negative_slope=1e-2);
