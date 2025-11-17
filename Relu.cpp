28ea399cea3fcb00bc16ed2d2d9c57029e9b235f
ZYROS-OFFICIEL <rondomiez@gmail.com>
2025-11-17 23:48:55 +0100

    add LeakyRelu function implementation in Relu.cpp

---
+++ Relu.cpp
@@ -0,0 +1,25 @@
+#include "Relu.h"
+
+
+Tensor LeakyRelu(const Tensor& a_,double negative_slope ){
+    if (!a_.impl)
+        throw std::runtime_error("Relu_: null tensor implementation");
+        bool req = a_.requires_grad();
+
+    Tensor result(a_.impl->shape,a_.impl->ndim, a_.impl->dtype, req);
+    
+    if (req) {
+        result.impl->grad_fn = std::make_shared<GradRelu>(a_);
+    }
+
+    auto* a_data = a_.impl->storage->data.get();
+    auto* r_data = result.impl->storage->data.get();
+
+    for (size_t i = 0; i < n; ++i) {
+        double val = read_scalar_at(a_data, i, a_.impl->dtype);
+        write_scalar_at(r_data, i, result.impl->dtype, ((val >= 0.0 )? val : val*negative_slope));
+    }
+
+    return result;
+
+}
