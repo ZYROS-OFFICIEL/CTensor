8d011045a5bf13db99ffc386f442168919e5600b
ZYROS-OFFICIEL <rondomiez@gmail.com>
2025-11-18 00:21:56 +0100

    fix PRelu error handling and correct parameter validation logic

--- Relu.cpp
+++ Relu.cpp
@@ -26,11 +26,22 @@ Tensor LeakyRelu(const Tensor& a_,double negative_slope ){
 Tensor PRelu(const Tensor& a_,DType init,int num_parameters,DType dtype){
     size_t in_c  = a_.impl->shape[1];
     if (!a_.impl)
-        throw std::runtime_error("Relu_: null tensor implementation");
+        throw std::runtime_error("PRelu_: null tensor implementation");
 
     bool req = a_.requires_grad();
     Tensor result(a_.impl->shape,a_.impl->ndim, a_.impl->dtype, req);
-    if(num_parameters == in_c && num_parameters>1){
+    if(num_parameters /= in_c && num_parameters>1){
+        throw std::runtime_error("PRelu_: RuntimeError: mismatch in shape between input and weight");
+    }else{
+        auto* a_data = a_.impl->storage->data.get();
+        auto* r_data = result.impl->storage->data.get();
+
+        for (size_t i = 0; i < n; ++i) {
+            double val = read_scalar_at(a_data, i, a_.impl->dtype);
+            write_scalar_at(r_data, i, result.impl->dtype, ((val >= 0.0 )? val : val*init));
+        }
+
+        return result;
 
     }
 
