e7648d6b61eb3bd6810ceeb2c8089f6802021ff4
ZYROS-OFFICIEL <rondomiez@gmail.com>
2025-11-18 01:54:10 +0100

    add GradLeakyRelu and GradPRelu structs with backward methods for gradient computation

--- Relu.h
+++ Relu.h
@@ -1,13 +1,39 @@
-#include "tensor1.h"
-#include <vector>
-#include <stdexcept>
-#include <cmath>
-#include "autograd.h"
-#include <immintrin.h>
-#include <cstring>
-#include "ops1.h"
-#include <string>
 #pragma once
+#include "tensor1.h"
+#include "autograd.h"
+#include <string>
 
-Tensor LeakyRelu(const Tensor& a_,double negative_slope=1e-2);
-Tensor PRelu(const Tensor& a_,DType init=0.25,int num_parameters=1,DType dtype=DType::Double64);
+Tensor LeakyRelu(const Tensor& a_, double negative_slope = 0.01);
+
+struct GradLeakyRelu : GradFn {
+    Tensor a;
+    double negative_slope;
+    GradLeakyRelu(const Tensor& a_, double neg_slope) 
+        : a(a_), negative_slope(neg_slope) {
+        parents = {a};
+    }
+    void backward(const Tensor& self) override;
+};
+
+
+
+class PRelu {
+public:
+    Tensor weight;
+    int num_parameters; 
+
+    PRelu(int num_parameters = 1, double init = 0.25, DType dtype = DType::Float32);
+
+    Tensor forward(const Tensor& input);
+    Tensor operator()(const Tensor& input) { return forward(input); }
+};
+
+struct GradPRelu : GradFn {
+    Tensor input, weight;
+    
+    GradPRelu(const Tensor& input_, const Tensor& weight_) 
+        : input(input_), weight(weight_) {
+        parents = {input, weight}; 
+    }
+    void backward(const Tensor& self) override;
+};
