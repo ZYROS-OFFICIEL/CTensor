313e62381de0656f58cc73b3dcb521dc55318fac
ZYROS-OFFICIEL <rondomiez@gmail.com>
2025-11-20 00:09:13 +0100

    add Dropout layer implementation for regularization during training

---
+++ dropout.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "tensor1.h"
+#include "autograd.h"
+
+// --- Dropout Layer ---
+// Randomly zeros some of the elements of the input tensor with probability p.
+// Used for regularization during training.
+class Dropout {
+public:
+    double p; // probability of an element to be zeroed
+    
+    Dropout(double p = 0.5);
+
+    Tensor forward(const Tensor& input);
+    Tensor operator()(const Tensor& input) { return forward(input); }
+};
