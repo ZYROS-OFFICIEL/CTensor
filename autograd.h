3f7537c61a4e7dea77d8d99c2c63698c52f485f5
ZYROS-OFFICIEL <rondomiez@gmail.com>
2025-11-24 23:29:27 +0100

    refactor: update header inclusion in autograd.h and add opsmp.cpp for tensor operations

--- autograd.h
+++ autograd.h
@@ -11,7 +11,7 @@
 #include <cmath>
 
 #include "tensor1.h"
-#include "ops1.h"
+#include "opsmp.h"
 
 // -------------------- helpers --------------------
 inline void check_index_in_storage(const Tensorimpl* impl, size_t idx, const char* ctx) {
