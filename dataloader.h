60e3ff85a2d369b1cdea07ef845b79fb712d9ddd
ZYROS-OFFICIEL <rondomiez@gmail.com>
2025-12-04 23:28:01 +0100

    adding method to load data

--- dataloader.h
+++ dataloader.h
@@ -16,4 +16,18 @@ class DataLoader {
     // Target DTypes
     DType img_dtype;
     DType lbl_dtype;
+public:
+    // Default to Float32/Int32 if not specified, or match dataset?
+    // Let's allow user to specify.
+    DataLoader(MNISTData& dataset, size_t bs, bool shuffle_data = true, 
+               DType image_type = DType::Float32, DType label_type = DType::Int32) 
+        : data(dataset), batch_size(bs), current_idx(0), shuffle(shuffle_data),
+          img_dtype(image_type), lbl_dtype(label_type)
+    {
+        size_t n = data.images.shape()[0];
+        indices.resize(n);
+        for (size_t i = 0; i < n; ++i) indices[i] = i;
+        reset();
+    }
+
 };
