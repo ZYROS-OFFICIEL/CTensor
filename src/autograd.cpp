7e9a86ad83755e50457958aff11c287aecf16fd2
ZYROS-OFFICIEL <rondomiez@gmail.com>
2025-12-18 23:51:18 +0100

    Adding GradSubScalar::backward()

--- src/autograd.cpp
+++ src/autograd.cpp
@@ -219,7 +219,9 @@ void GradPow::backward(const Tensor& self) {
 void GradAddScalar::backward(const Tensor& self) {
     if (a.requires_grad()) accumulate_grad(a, tensor_from_grad(self));
 }
-
+void GradSubScalar::backward(const Tensor& self) {
+    if (a.requires_grad()) accumulate_grad(a, tensor_from_grad(self));
+}
 
 
 
