0bcc1783d287d9f0af41f5660b34bd6f2dd85b7f
ZYROS-OFFICIEL <rondomiez@gmail.com>
2025-12-19 21:53:57 +0100

    Adding GradTanh::backward()

--- src/autograd.cpp
+++ src/autograd.cpp
@@ -409,6 +409,11 @@ void GradCosh::backward(const Tensor& s){
         accumulate_grad(t, Ops::mul(tensor_from_grad(s), Ops::sinh(t))); 
     }
 }
+void GradTanh::backward(const Tensor& s){ 
+    if(t.requires_grad()) {
+        accumulate_grad(t, Ops::mul(tensor_from_grad(s), Ops::div_scalar_rev(1.0, Ops::mul(Ops::cosh(t), Ops::cosh(t))))); 
+    }
+}
 
 void backward(Tensor& root) {
     if (!root.impl || !root.requires_grad()) 
