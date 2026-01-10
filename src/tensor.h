c6ba80bd410f9074d7d0948ef43031b576956f01
ZYROS-OFFICIEL <rondomiez@gmail.com>
2026-01-10 02:08:11 +0100

    Rename dtype_to_string function to dtype_to_str and correct Float64 case to Double64

--- src/tensor.h
+++ src/tensor.h
@@ -355,10 +355,10 @@ inline void print_t(const Tensor& t) {
     std::cout << "]\n";
     
 }
-inline const char* dtype_to_string(DType dt) {
+inline const char* dtype_to_str(DType dt) {
     switch (dt) {
         case DType::Float32: return "float32";
-        case DType::Float64: return "float64";
+        case DType::Double64: return "double64";
         case DType::Int32:   return "int32";
         case DType::Int64:   return "int64";
         default:             return "unknown";
