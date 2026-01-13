b61689f35d9f00c9bba6796e63b155413b035092
ZYROS-OFFICIEL <rondomiez@gmail.com>
2026-01-14 00:06:31 +0100

    Updating Time AVX TEST

--- test/time_avx.cpp
+++ test/time_avx.cpp
@@ -2,11 +2,11 @@
 #include <chrono>
 #include "tensor.h"
 #include "opsmp.h"
-
+#include "ops_dispatch.h"
 main(){
     
-    Tensor a = Tensor::ones({1000, 1000}, DType::Float64);
-    Tensor b = Tensor::ones({1000, 1000}, DType::Float64);
+    Tensor a = Tensor::rand({1000, 1000}, DType::Float32);
+    Tensor b = Tensor::rand({1000, 1000}, DType::Float32);
     auto start1 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 100; ++i) {
         Tensor c = add_mp(a, b);
@@ -20,7 +20,7 @@ main(){
     }
     auto end2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed2 = end2 - start2;
-    std::cout << "Time taken for 100 Scalar additions of 1000x1000 tensors: " << elapsed2.count() << " seconds\n";
+    std::cout << "Time taken for 100 Avx additions of 1000x1000 tensors: " << elapsed2.count() << " seconds\n";
 
     std::cout << "RAPIDITY : " << elapsed1.count() / elapsed2.count() << " \n";
 
