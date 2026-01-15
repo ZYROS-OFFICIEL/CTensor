233069273be34af7380e036d1c4b20ce8fb6ba84
ZYROS-OFFICIEL <rondomiez@gmail.com>
2026-01-15 22:47:55 +0100

    Declaring GradCrossEntropy and GradBCE

--- src/neurtalnet/loss.h
+++ src/neurtalnet/loss.h
@@ -62,3 +62,18 @@ struct GradHuberLoss : GradFn {
     void backward(const Tensor& self) override;
 };
 
+struct GradCrossEntropy : GradFn {
+    Tensor pred, target;
+    std::string reduction;
+    GradCrossEntropy(const Tensor& pred_, const Tensor& target_, const std::string& reduction_)
+        : pred(pred_), target(target_), reduction(reduction_) { parents = {pred}; }
+    void backward(const Tensor& self) override;
+};
+
+struct GradBCE : GradFn {
+    Tensor pred, target;
+    std::string reduction;
+    GradBCE(const Tensor& pred_, const Tensor& target_, const std::string& reduction_)
+        : pred(pred_), target(target_), reduction(reduction_) { parents = {pred}; }
+    void backward(const Tensor& self) override;
+};
