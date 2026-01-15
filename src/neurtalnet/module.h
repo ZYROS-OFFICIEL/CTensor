af3e238704af482fe66527e6b2d9f55cad24aa71
ZYROS-OFFICIEL <rondomiez@gmail.com>
2026-01-15 22:42:23 +0100

    Declaring Sequential class

--- src/neurtalnet/module.h
+++ src/neurtalnet/module.h
@@ -24,3 +24,35 @@ public:
         return {}; // Default implementation returns nothing
     }
 };
+
+// A container that holds a list of modules
+class Sequential : public Module {
+public:
+    std::vector<std::shared_ptr<Module>> modules;
+
+    void add(std::shared_ptr<Module> module) {
+        modules.push_back(module);
+    }
+
+    // Recursive train/eval
+    void train() override {
+        training = true;
+        for (auto& m : modules) m->train();
+    }
+
+    void eval() override {
+        training = false;
+        for (auto& m : modules) m->eval();
+    }
+    
+    // Recursive parameter collection
+    std::vector<Tensor*> parameters() override {
+        std::vector<Tensor*> params;
+        for (auto& m : modules) {
+            auto p = m->parameters();
+            params.insert(params.end(), p.begin(), p.end());
+        }
+        return params;
+    }
+    
+};
