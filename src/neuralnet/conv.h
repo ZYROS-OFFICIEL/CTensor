66e09225bb69c100150f4386565ea074295a8d4d
ZYROS-OFFICIEL <rondomiez@gmail.com>
2026-01-19 23:31:31 +0100

    Declaring : Conv3d layer

--- src/neuralnet/conv.h
+++ src/neuralnet/conv.h
@@ -32,3 +32,20 @@ public:
     Tensor forward(const Tensor& input);
     Tensor operator()(const Tensor& input) { return forward(input); }
 };
+
+// --- Conv3d ---
+class Conv3d : public Module {
+public:
+    int in_channels, out_channels;
+    int kernel_size_d, kernel_size_h, kernel_size_w;
+    int stride_d, stride_h, stride_w;
+    int padding_d, padding_h, padding_w;
+    Tensor weight, bias;
+
+    // Added DType
+    Conv3d(int in_c, int out_c, int kd, int kh, int kw, int sd = 1, int sh = 1, int sw = 1, int pd = 0, int ph = 0, int pw = 0, DType dt = DType::Float32);
+
+    std::vector<Tensor*> parameters() override { return {&weight, &bias}; }
+    Tensor forward(const Tensor& input);
+    Tensor operator()(const Tensor& input) { return forward(input); }
+};
