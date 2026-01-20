9d1c695fdd43795e41ddd5c6efa08619640ab2b7
ZYROS-OFFICIEL <rondomiez@gmail.com>
2026-01-20 22:52:02 +0100

    Declaring : MaxPool2d

--- src/neuralnet/pooling/pooling.h
+++ src/neuralnet/pooling/pooling.h
@@ -18,3 +18,17 @@ public:
     Tensor forward(const Tensor& input);
     Tensor operator()(const Tensor& input) { return forward(input); }
 };
+
+// --- MaxPool2d ---
+class MaxPool2d : public Module {
+public:
+    int kernel_size_h, kernel_size_w;
+    int stride_h, stride_w;
+    int padding_h, padding_w;
+
+    // FIX: Default strides to -1
+    MaxPool2d(int kh, int kw, int sh = -1, int sw = -1, int ph = 0, int pw = 0);
+
+    Tensor forward(const Tensor& input);
+    Tensor operator()(const Tensor& input) { return forward(input); }
+};
