1fc25bc7fd58dc0664d6572e1cbab88e928708f4
ZYROS-OFFICIEL <rondomiez@gmail.com>
2026-01-22 01:48:57 +0100

    speed test

--- test/time_avx.cpp
+++ test/time_avx.cpp
@@ -7,16 +7,18 @@ main(){
     
     Tensor a = Tensor::rand({1000, 1000}, DType::Float32);
     Tensor b = Tensor::rand({1000, 1000}, DType::Float32);
+    Tensor c;
     auto start1 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 100; ++i) {
-        Tensor c = add_mp(a, b);
+        c = add_mp(a, b);
     }
     auto end1 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed1 = end1 - start1;
     std::cout << "Time taken for 100 Scalar additions of 1000x1000 tensors: " << elapsed1.count() << " seconds\n";
     auto start2 = std::chrono::high_resolution_clock::now();
+
     for (int i = 0; i < 100; ++i) {
-        Tensor c = add(a, b);
+        c = add(a, b);
     }
     auto end2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed2 = end2 - start2;
