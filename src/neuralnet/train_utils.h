e6ac4803ac346e22be19bed6127a01342bacb2e3
ZYROS-OFFICIEL <rondomiez@gmail.com>
2026-01-31 18:44:22 +0100

    Adding : check for gradiets

--- src/neuralnet/train_utils.h
+++ src/neuralnet/train_utils.h
@@ -36,6 +36,9 @@ public:
 
     void zero_grad() {
         for (auto* p : params) {
+        if (!p->impl) continue;
+
+        if (!p->impl->grad) continue;
             if (p->impl->grad->data->data) {
                 size_t nbytes = p->numel() * p->dtype_bytes();
                 std::memset(p->impl->grad->data->data.get(), 0, nbytes);
