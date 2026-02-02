635fa35aa42fa4822a304aaddc5db75e922829c9
ZYROS-OFFICIEL <rondomiez@gmail.com>
2026-02-02 23:02:13 +0100

    Declaring : transformation tensors

---
+++ src/core/transforme.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+#include <functional>
+#include "tensor.h"
+
+struct Transforme {
+    // pipeline of tensor transformations
+    std::vector<std::function<Tensor(const Tensor&)>> pipeline;
+
+    // ----- Add a normalization operation to the pipeline -----
+    void normalize_(const std::vector<float>& mean, const std::vector<float>& stdv);
+
+    // ----- Add a resize operation to the pipeline -----
+    void resize_(size_t H, size_t W);
+
+    // ----- Apply all transformations in the pipeline -----
+    Tensor operator()(const Tensor& input) const;
+};
