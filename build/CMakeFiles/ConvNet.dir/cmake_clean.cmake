469050c6bb0540ee0a8657faf6a675bba4cdb5ed
ZYROS <rondomiez@gmail.com>
2026-03-04 01:14:28 +0000

    Building the test project

---
+++ build/CMakeFiles/ConvNet.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ConvNet.dir/link.d"
+  "CMakeFiles/ConvNet.dir/test/train_conv.cpp.o"
+  "CMakeFiles/ConvNet.dir/test/train_conv.cpp.o.d"
+  "ConvNet"
+  "ConvNet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ConvNet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ConvNet.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ConvNet.
+# This may be replaced when dependencies are built.
