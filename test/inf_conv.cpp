628e7dc36941c31696cf981ec9a0d724add81d5f
ZYROS <rondomiez@gmail.com>
2026-03-07 21:50:00 +0000

    Refactoring the inference model

--- test/inf_conv.cpp
+++ test/inf_conv.cpp
@@ -7,35 +7,41 @@ using namespace torch;
 
 class ConvNet : public nn::Module {
 public:
-    // FIX: Added the 4th argument (kernel_size_w) to match your custom Conv2d constructor.
-    // Signature: Conv2d(in_channels, out_channels, kernel_size_h, kernel_size_w)
+    // Block 1
     nn::Conv2d conv1{1, 16, 3, 3};  
-    nn::Conv2d conv2{16, 32, 3, 3};
-    nn::Conv2d conv3{32, 64, 3, 3};
-    
-    // MaxPool2d requires kernel dimensions (from pooling.h: MaxPool2d(kh, kw))
     ::MaxPool2d maxpool1{2, 2};  
+
+    // Block 2
+    nn::Conv2d conv2{16, 32, 3, 3};
+    ::MaxPool2d maxpool2{2, 2};  
     
     nn::Flatten flat;
     
-    // MNIST input is 28x28.
-    // After Conv1 (3x3, pad=0): 28 -> 26
-    // After Conv2 (3x3, pad=0): 26 -> 24
-    // After Conv3 (3x3, pad=0): 24 -> 22
-    // After MaxPool1 (2x2): 22 / 2 = 11
-    // Final feature map shape: [Batch, 64 channels, 11 height, 11 width]
-    nn::Linear fc1{64 * 11 * 11, 10}; 
+    // New Dimension Math:
+    // Input: 28x28
+    // conv1 (3x3): 28 -> 26
+    // maxpool1 (2x2): 26 / 2 = 13
+    // conv2 (3x3): 13 -> 11
+    // maxpool2 (2x2): 11 / 2 = 5  (integer division: 11/2 = 5)
+    // Final feature map shape: [Batch, 32 channels, 5 height, 5 width]
+    nn::Linear fc1{32 * 5 * 5, 128}; 
+    nn::Linear fc2{128, 10}; 
 
     Tensor forward(const Tensor& x) {
-        Tensor out = nn::functional::relu(conv1(x));
-        out = nn::functional::relu(conv2(out));
-        out = nn::functional::relu(conv3(out));
-        
+        // Block 1
+        Tensor out = conv1(x);
+        out = nn::functional::relu(out);
         out = maxpool1(out);
         
-        out = flat(out); 
+        // Block 2
+        out = conv2(out);
+        out = nn::functional::relu(out);
+        out = maxpool2(out);
         
-        return fc1(out);
+        // Classifier
+        out = flat(out); 
+        out = nn::functional::relu(fc1(out));
+        return fc2(out);
     }
 
     Tensor operator()(const Tensor& x) { 
@@ -43,11 +49,9 @@ public:
     }
 
     std::vector<Tensor*> parameters() override {
-        return nn::combine_params(conv1, conv2, conv3, fc1);
+        return nn::combine_params(conv1, conv2, fc1, fc2);
     }
 };
-
-
 int main() {
     omp_set_num_threads(4);
     std::cout << "PyTorch-Style CTensor MNIST Training\n";
