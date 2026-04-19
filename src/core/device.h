27cd17a42dd6e3d3c698a85e6bbee5b6cc575d59
ZYROS <rondomiez@gmail.com>
2026-04-19 23:55:21 +0100

    Adding :Macros to device for cross compilation

--- src/core/device.h
+++ src/core/device.h
@@ -2,24 +2,30 @@
 #include <string>
 #include <stdexcept>
 
+#ifdef __CUDACC__
+    #define HOST_DEVICE __host__ __device__
+#else
+    #define HOST_DEVICE
+#endif
+
 enum class DeviceType { CPU, CUDA };
 
 struct Device {
     DeviceType type;
-    int index; // e.g., GPU 0, GPU 1
+    int index; 
 
-    Device(DeviceType t = DeviceType::CPU, int i = -1) : type(t), index(i) {}
+    HOST_DEVICE Device(DeviceType t = DeviceType::CPU, int i = -1) : type(t), index(i) {}
     
-    bool is_cpu() const { return type == DeviceType::CPU; }
-    bool is_cuda() const { return type == DeviceType::CUDA; }
+    HOST_DEVICE bool is_cpu() const { return type == DeviceType::CPU; }
+    HOST_DEVICE bool is_cuda() const { return type == DeviceType::CUDA; }
     
     std::string str() const {
         return (type == DeviceType::CPU ? "cpu" : "cuda") + 
                (index >= 0 ? ":" + std::to_string(index) : "");
     }
     
-    bool operator==(const Device& other) const {
+    HOST_DEVICE bool operator==(const Device& other) const {
         return type == other.type && index == other.index;
     }
-    bool operator!=(const Device& other) const { return !(*this == other); }
+    HOST_DEVICE bool operator!=(const Device& other) const { return !(*this == other); }
 };
